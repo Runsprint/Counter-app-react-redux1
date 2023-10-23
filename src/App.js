3f0221b8431b283d2323d61b92a850db433ec1bf
gvantsa kapanadze <gvantsakapanadze95@gmail.com>
2023-10-23 23:56:39 +0400

    use reducer and finish counter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,58 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import "./App.css";
 
 function App() {
   const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+  const additional = () => {
+    dispatch({ type: "add" });
+  };
+  const minus = () => {
+    dispatch({ type: "min" });
+  };
   return (
-    <div>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        textAlign: "center",
+        flexDirection: "column",
+      }}
+    >
       <h1>Counter App</h1>
       <h2>{counter}</h2>
+      <button
+        onClick={additional}
+        style={{
+          height: "60px",
+          borderRadius: "20px",
+          width: "130px",
+          background: "pink",
+          border: "none",
+          fontSize: "30px",
+          color: "white",
+          marginBottom: "20px",
+        }}
+      >
+        Add
+      </button>
+      <button
+        onClick={minus}
+        style={{
+          height: "60px",
+          borderRadius: "20px",
+          width: "130px",
+          background: "red",
+          border: "none",
+          fontSize: "30px",
+          color: "white",
+          marginBottom: "20px",
+        }}
+      >
+        Minus
+      </button>
     </div>
   );
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,12 @@
 import { createStore } from "redux";
 
 const reducerFn = (state = { counter: 0 }, action) => {
+  if (action.type === "add") {
+    return { counter: state.counter + 1 };
+  }
+  if (action.type === "min") {
+    return { counter: state.counter - 1 };
+  }
   return state;
 };
 
